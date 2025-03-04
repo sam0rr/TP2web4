e4459dce15716b81c61101436dfc620433b58f5f
sam0rr <Samor140@hotmail.com>
2025-03-04 12:50:51 -0500

    fix sql formatting

--- sql/auth/structure.sql
+++ sql/auth/structure.sql
@@ -20,19 +20,19 @@ CREATE TABLE userToken
 );
 
 CREATE TABLE userWallet (
-      userId INT PRIMARY KEY,
-      balance NUMERIC(10,2) DEFAULT 0 CHECK (balance >= 0),
-      totalSpent NUMERIC(10,2) DEFAULT 0,
-      FOREIGN KEY (userId) REFERENCES userProfile(id) ON DELETE CASCADE
+    userId INT PRIMARY KEY,
+    balance NUMERIC(10,2) DEFAULT 0 CHECK (balance >= 0),
+    totalSpent NUMERIC(10,2) DEFAULT 0,
+    FOREIGN KEY (userId) REFERENCES userProfile(id) ON DELETE CASCADE
 );
 
 CREATE TABLE transaction (
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  userId INT NOT NULL,
-  itemName TEXT NOT NULL,
-  price NUMERIC(10,2) NOT NULL CHECK (price >= 0),
-  quantity INT NOT NULL CHECK (quantity > 0),
-  totalPrice NUMERIC(10,2) GENERATED ALWAYS AS (price * quantity) STORED,
-  createdAt TIMESTAMP DEFAULT NOW(),
-  FOREIGN KEY (userId) REFERENCES userProfile(id) ON DELETE CASCADE
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    userId INT NOT NULL,
+    itemName TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL CHECK (price >= 0),
+    quantity INT NOT NULL CHECK (quantity > 0),
+    totalPrice NUMERIC(10,2) GENERATED ALWAYS AS (price * quantity) STORED,
+    createdAt TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (userId) REFERENCES userProfile(id) ON DELETE CASCADE
 );
